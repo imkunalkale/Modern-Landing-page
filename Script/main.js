9c65ba51eef639af461e689a979221f72052cc19
kunal kale <kunalkale7080@gmail.com>
2023-10-15 15:58:48 +0530

    commit

--- Script/main.js
+++ Script/main.js
@@ -24,3 +24,25 @@ Shery.imageEffect("#back", {
   },
   gooey: true,
 });
+
+var h1s = document.querySelectorAll("h1");
+var index = 0;
+
+document.querySelector("#main").addEventListener("click", function () {
+  gsap.to(h1s[index], {
+    top: "-=100%",
+    ease: Expo.easeInOut,
+    duration: 1,
+    onComplete: function () {
+      gsap.set(this._targets[0], { top: "100%" });
+    },
+  });
+
+  index === h1s.length - 1 ? (index = 0) : index++;
+
+  gsap.to(h1s[index], {
+    top: "-=100%",
+    ease: Expo.easeInOut,
+    duration: 1,
+  });
+});
