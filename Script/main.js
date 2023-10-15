da20f104571ad6da29d92dd030aded474e2aad31
kunal kale <kunalkale7080@gmail.com>
2023-10-15 16:46:30 +0530

    commits

--- Script/main.js
+++ Script/main.js
@@ -25,24 +25,33 @@ Shery.imageEffect("#back", {
   gooey: true,
 });
 
-var h1s = document.querySelectorAll("h1");
-var index = 0;
+var elems = document.querySelectorAll(".elem");
 
-document.querySelector("#main").addEventListener("click", function () {
-  gsap.to(h1s[index], {
-    top: "-=100%",
-    ease: Expo.easeInOut,
-    duration: 1,
-    onComplete: function () {
-      gsap.set(this._targets[0], { top: "100%" });
-    },
-  });
+elems.forEach(function (elem) {
+  var h1s = elem.querySelectorAll("h1");
+  var index = 0;
+  var animating = false;
 
-  index === h1s.length - 1 ? (index = 0) : index++;
+  document.querySelector("#main").addEventListener("click", function () {
+    if (!animating) {
+        animating = true;
+      gsap.to(h1s[index], {
+        top: "-=100%",
+        ease: Expo.easeInOut,
+        duration: 1,
+        onComplete: function () {
+          gsap.set(this._targets[0], { top: "100%" });
+          animating = false;
+        },
+      });
 
-  gsap.to(h1s[index], {
-    top: "-=100%",
-    ease: Expo.easeInOut,
-    duration: 1,
+      index === h1s.length - 1 ? (index = 0) : index++;
+
+      gsap.to(h1s[index], {
+        top: "-=100%",
+        ease: Expo.easeInOut,
+        duration: 1,
+      });
+    }
   });
 });
